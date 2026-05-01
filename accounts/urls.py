97ce670e9366d12f0e54c954fe173defba995a3f
Parshkhadka07 <parashkhadka020@gmail.com>
2026-05-01 15:09:31 +0545

    login-logout implemented

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,8 @@ from accounts import views
 
 urlpatterns=[
     path("register/",views.register,name="registration_page"),
-    path("login/",views.login,name="login_page"),
+    path("login/",views.login_view,name="login_page"),
+    path("logout/",views.logout_view,name="logout_page"),
+
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,35 @@
 from django.shortcuts import render,redirect
 from .admin import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
+
+def login_view(request):
+    if request.method=="POST":
+        user=authenticate(request, email=request.POST.get("email"),password=request.POST.get("password"))
+        if user is not None:
+            login(request,user)
+            messages.success(request,"Login Sucessfull !")
+            return redirect("home_page")
+        else:
+            messages.error(request,"Email or Password did not match !")
+            return redirect("login_page")
+
+    else:   
+        return render(request,"accounts/login.html")
+
+
+
+
 def register(request):
-    print("***********************",request.POST,"**********************")
     if request.method == "POST":
         submitted_form=UserCreationForm(request.POST)
         
         if submitted_form.is_valid():
             submitted_form.save()
-            messages.success(request,"registration sucessful")
-            return redirect("home_page")
+            messages.success(request,"Registration Sucessful !")
+            return redirect("login_page")
         else:
             return render(request, "accounts/registration.html", {"form": submitted_form})
     else:
@@ -19,7 +37,7 @@ def register(request):
 
         
     
-
-def login(request):
-    return render(request,"accounts/login.html")
+def logout_view(request):
+    logout(request)
+    return redirect("home_page")
     
