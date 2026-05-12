9bf6ad50c91130e01fc396c62b898adf573757ee
Parshkhadka07 <parashkhadka020@gmail.com>
2026-05-12 22:49:35 +0545

    added

--- main/views.py
+++ main/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from accounts.models import MyUser
 # Create your views here.
 def home(request):
+    query = request.GET.get('category')
+    
     photographers=MyUser.objects.filter(user_role='photographer',profile__kyc_verified="verified")
+    if query:
+        photographers = photographers.filter(
+            profile__specialization=query
+        )
     context={"photographers":photographers}
     return render(request,"main/home.html",context)
